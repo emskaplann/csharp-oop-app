9fef47289a9a07a84e7f24960dcd485a36e74bc5
Emirhan Kaplan <emskaplann@gmail.com>
2021-02-06 20:49:03 -0500

    files added.

---
+++ BankAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; } // This how you give permission for operation on the object(get, set).
+        public string Owner { get; set; }
+        public decimal Balance {
+            get // Here is how you set a getter in C#.
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumberSeed = 100000000;
+
+        private List<Transaction> allTransactions = new List<Transaction> { };
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            MakeDeposit(initialBalance, DateTime.Now, "initial deposit.");
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Owner: {this.Owner}, Balance: ${this.Balance}, Account Number: #{this.Number}");
+        }
+
+        public void PrintTransactions()
+        {
+            var report = new StringBuilder();
+            
+            // Header Line
+            report.AppendLine("Idx\tDate\t\tAmount\tNote");
+            Console.WriteLine("Transactions:");
+            int index = 1;
+            foreach (Transaction transaction in allTransactions)
+            {
+                report.AppendLine($"{index}\t{transaction.Date.ToShortDateString()}\t{transaction.Amount}\t{transaction.Note}");
+                index++;
+            }
+            Console.Write(report.ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Intro to Object Oriented Programming with C#
+            BankAccount newAccount = new BankAccount("Emirhan", 1453);
+            newAccount.Print();
+            newAccount.MakeWithdrawal(1119, DateTime.Now, "New laptop.");
+            newAccount.Print();
+
+            Console.WriteLine();
+            newAccount.PrintTransactions();
+            Console.WriteLine();
+
+            // Exception Handling
+            Console.WriteLine("Attempting to overdraw...");
+            // Test for a negative balance.
+            try
+            {
+                newAccount.MakeWithdrawal(540, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Trying to create invalid account.");
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
